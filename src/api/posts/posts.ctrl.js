27e5cfa0c2e7d2ad0b9e4b418525013c4e4515f1
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-31 10:13:05 +0900

    8th commit

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -55,8 +55,27 @@ export const write = async (ctx) => {
   GET /api/posts
 */
 export const list = async (ctx) => {
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const posts = await Post.find().exec();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
     ctx.body = posts;
   } catch (e) {
     ctx.throw(500, e);
